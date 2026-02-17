8c8bf9223d019fd3a33f44795211822049e9fe7e
Harsh Joshi <harshcode0907@gmail.com>
2026-02-17 23:42:36 +0530

    created verifyOtp page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
 	Card,
@@ -9,9 +10,38 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { ArrowRight, Mail } from "lucide-react";
+import axios from "axios";
+import { ArrowRight, Loader2, Mail } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+interface LoginResponse {
+	message: string;
+}
 
 const Page = () => {
+	const [email, setEmail] = useState<string>("");
+	const [loading, setLoading] = useState<boolean>(false);
+	const router = useRouter();
+	const handleSubmit = async (
+		e: React.FormEvent<HTMLFormElement>,
+	): Promise<void> => {
+		e.preventDefault();
+		setLoading(true);
+		try {
+			const { data } = await axios.post<LoginResponse>(
+				`http://localhost:5000/api/v1/login`,
+				{
+					email,
+				},
+			);
+			alert(data.message);
+			router.push(`/verify?email=${email}`);
+		} catch (error: any) {
+			alert(error.response?.data.message || "Something went wrong");
+		} finally {
+			setLoading(false);
+		}
+	};
 	return (
 		<div className="min-h-screen flex justify-center items-center px-4">
 			<Card className="w-full max-w-sm">
@@ -20,9 +50,7 @@ const Page = () => {
 						<Mail size={50} className="text-primary" />
 					</div>
 
-					<CardTitle className="text-xl">
-						Welcome to WebChat
-					</CardTitle>
+					<CardTitle className="text-xl">Welcome to WebChat</CardTitle>
 
 					<CardDescription className="text-sm">
 						Enter your email below to continue
@@ -30,7 +58,7 @@ const Page = () => {
 				</CardHeader>
 
 				<CardContent>
-					<form className="space-y-4">
+					<form className="space-y-4" onSubmit={handleSubmit}>
 						<div className="grid gap-1.5">
 							<Label htmlFor="email">Email</Label>
 							<Input
@@ -38,13 +66,22 @@ const Page = () => {
 								type="email"
 								placeholder="m@example.com"
 								required
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
 							/>
 						</div>
-
-						<Button type="submit" className="w-full">
-							Send Verification code
-							<ArrowRight className="ml-2 h-4 w-4" />
-						</Button>
+						{loading ? (
+							<div className="flex items-center justify-center gap-2">
+								<Loader2>
+									Sending otp to your mail...
+								</Loader2>
+							</div>
+						) : (
+							<Button type="submit" className="w-full" disabled={loading}>
+								Send Verification code
+								<ArrowRight className="ml-2 h-4 w-4" />
+							</Button>
+						)}
 					</form>
 				</CardContent>
 
@@ -59,5 +96,3 @@ const Page = () => {
 };
 
 export default Page;
-
-
