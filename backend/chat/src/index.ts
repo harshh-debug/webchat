52dfe07e6e833fd76d37d9be3d807b5701dbdf19
Harsh Joshi <harshcode0907@gmail.com>
2026-02-24 23:20:41 +0530

    implemented socket.io

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -3,13 +3,13 @@ import dotenv  from 'dotenv';
 import express from "express"
 import connectDb from './config/db.js';
 import chatRouter from './routes/chatRouter.js';
+import { app,server } from './config/socket.js';
 dotenv.config()
 connectDb()
-const app= express()
 app.use(express.json())
 app.use(cors())
 app.use("/api/v1",chatRouter)
 const port = process.env.PORT
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Chat service is running at ${port}`)
 })
--- frontend/components/Providers.tsx
+++ frontend/components/Providers.tsx
@@ -3,18 +3,15 @@
 import { SessionProvider } from "next-auth/react";
 import { AppProvider } from "@/context/AppContext";
 import OAuthTokenSync from "@/components/OAuthTokenSync";
+import { SocketProvider } from "@/context/SocketContext";
 
-export default function Providers({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <SessionProvider>
-      <AppProvider>
-        <OAuthTokenSync />
-        {children}
-      </AppProvider>
-    </SessionProvider>
-  );
+export default function Providers({ children }: { children: React.ReactNode }) {
+	return (
+		<SessionProvider>
+			<AppProvider>
+				<OAuthTokenSync />
+				<SocketProvider>{children}</SocketProvider>
+			</AppProvider>
+		</SessionProvider>
+	);
 }
