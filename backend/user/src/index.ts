257b2d45889c8133ff8a27c02da79f5a65ef81f6
Harsh Joshi <harshcode0907@gmail.com>
2026-06-27 16:57:08 +0530

    fix

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -27,7 +27,10 @@ redisClient
 
 const app = express();
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}))
 
 app.use("/api/v1",userRouter)
 
